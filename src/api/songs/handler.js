36eb3e74160409d033f21fe5bbf56c63f5a400ef
Darma Yasa <darmayasa@192.168.204.108>
2022-05-22 22:49:11 +0800

    added PUT /songs/{id}

--- src/api/songs/handler.js
+++ src/api/songs/handler.js
@@ -6,6 +6,7 @@ class SongsHandlers {
     this.postSongHandler = this.postSongHandler.bind(this);
     this.getSongsHandler = this.getSongsHandler.bind(this);
     this.getSongByIdHandler = this.getSongByIdHandler.bind(this);
+    this.putSongByIdHandler = this.putSongByIdHandler.bind(this);
   }
 
   async postSongHandler({ payload }, h) {
@@ -32,13 +33,23 @@ class SongsHandlers {
   }
 
   async getSongByIdHandler({ params }, h) {
-    await this._services.verifySongsId(params.id);
+    await this._services.verifySongId(params.id);
     const song = await this._services.getSongById(params.id);
     return h.response({
       status: 'success',
       data: {
         song,
       },
+    });
+  }
+
+  async putSongByIdHandler({ payload, params }, h) {
+    await this._services.verifySongId(params.id);
+    this._validator.validateSongPayload(payload);
+    await this._services.editSongById({ id: params.id, ...payload });
+    return h.response({
+      status: 'success',
+      message: 'song been edited',
     });
   }
 }
