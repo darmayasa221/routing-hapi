021539c665285ffc04f8dce2cb6a6384e6762ab1
Darma Yasa <darmayasa@192.168.204.108>
2022-05-22 18:32:08 +0800

    added migrate table songs

---
+++ migrations/1653214062237_create-table-songs.js
@@ -0,0 +1,48 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createTable('songs', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+      notNull: true,
+    },
+    title: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    year: {
+      type: 'SMALLINT',
+      notNull: true,
+    },
+    genre: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    performer: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    duration: {
+      type: 'INT',
+      notNull: false,
+    },
+    album_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+  });
+  pgm.addConstraint(
+    'songs',
+    'fk_songs.album_id_albums.id',
+    'FOREIGN KEY(album_id) REFERENCES albums(id) ON DELETE CASCADE',
+  );
+};
+
+exports.down = (pgm) => {
+  pgm.dropConstraint(
+    'songs',
+    'fk_songs.album_id_albums.id',
+  );
+  pgm.dropTable('songs');
+};
