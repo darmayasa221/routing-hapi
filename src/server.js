3d6d0e1ea82976f286505a173c811758205ef7a8
Darma Yasa <darmayasa@192.168.204.108>
2022-05-22 16:51:35 +0800

    added plugin  albums services and albums validator

--- src/server.js
+++ src/server.js
@@ -1,11 +1,26 @@
 const Hapi = require('@hapi/hapi');
+// routes
+const albums = require('./api/albums/index');
+// services
+const AlbumsServices = require('./services/postgres/AlbumsServices');
+// validator
+const AlbumsValidator = require('./validator/albums');
 
 const init = async () => {
+  const albumsService = new AlbumsServices();
   const server = Hapi.server({
     host: process.env.HOST,
     port: process.env.PORT,
   });
-
+  await server.register([
+    {
+      plugin: albums,
+      options: {
+        service: albumsService,
+        validator: AlbumsValidator,
+      },
+    },
+  ]);
   await server.start();
   // eslint-disable-next-line no-console
   console.log(`server runing on ${server.info.uri}`);
